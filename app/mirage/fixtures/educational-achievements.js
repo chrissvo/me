c70a3f1551caa20cae806fcea29fa15c2177ffa2
Chrissvo <git@christian.surf>
2015-10-18 20:18:47 +0200

    Write educational-achievements fixture

---
+++ app/mirage/fixtures/educational-achievements.js
@@ -0,0 +1,34 @@
+export default [
+
+  {
+    id: 1,
+    title: 'Postgraduate Certificate in International Business',
+    image: '',
+    description: 'Courses: Organizational Behavior, Managerial Accounting & Finance, Global Business',
+    organization: 'Mountbatten Institute (New York Program), St. Mary&quot;s University',
+    location: 'London, United Kingdom',
+    from: 'March 2014',
+    to: 'March 2015'
+  },
+  {
+    id: 2,
+    title: 'Pre-master program in Business Economics',
+    image: '',
+    description: 'Courses: Microeconomics, Macroeconomics, Corporate Finance, and Econometrics',
+    organization: 'Utrecht School of Economics, Utrecht University',
+    location: 'Utrecht, Netherlands',
+    from: 'September 2012',
+    to: 'August 2013'
+  },
+  {
+    id: 3,
+    title: 'Bachelor Liberal Arst and Sciences - University College (cum laude)',
+    image: '',
+    description: 'Major in Economics, minor in Statistics, and minor in Anthropology',
+    organization: 'University College Roosevelt (Honors College of Utrecht University)',
+    location: 'Middelburg, Netherlands',
+    from: 'January 2009',
+    to: 'Janurary 2012'
+  }
+
+];
