94e2765b4a0fc756026606c97a3a4c0f627e4abf
Chrissvo <git@christian.surf>
2015-10-18 20:18:02 +0200

    Add additional information to work-experiences fixture

--- app/mirage/fixtures/work-experiences.js
+++ app/mirage/fixtures/work-experiences.js
@@ -3,44 +3,62 @@ export default [
   {
     id: 1,
     title: 'Front End Developer',
+    image: '',
     description: 'Development, data analysis, and optimization of white-label referral marketing solutions',
     organization: 'Member Get Member Company',
-    location: 'Amsterdam, Netherlands'
+    location: 'Amsterdam, Netherlands',
+    from: 'May 2015',
+    to: 'present'
   },
   {
     id: 2,
     title: 'Information Risk Analyst',
+    image: '',
     description: 'Audit remediation, automation, and project management',
     organization: 'JPMorgan Chase & Co.',
-    location: 'New York, United States'
+    location: 'New York, United States',
+    from: 'March 2014',
+    to: 'March 2015'
   },
   {
     id: 3,
     title: 'Founding partner',
+    image: '',
     description: 'Product development, marketing, strategy, HR, and administration',
     organization: 'eerstelinks.nl',
-    location: 'Amsterdam, Netherlands'
+    location: 'Amsterdam, Netherlands',
+    from: 'September 2013',
+    to: 'present'
   },
   {
     id: 4,
     title: 'Freelance consultant',
+    image: '',
     description: 'Excel modeling, web development, and online marketing',
     organization: 'SAMAN Groep',
-    location: 'Zierikzee, Netherlands'
+    location: 'Zierikzee, Netherlands',
+    from: 'December 2011',
+    to: 'present'
   },
   {
     id: 5,
     title: 'Business Consultant and Data Scientist',
+    image: '',
     description: 'Data analysis, reporting, and IT project management',
     organization: 'Europe Container Terminals',
-    location: 'Rotterdam (Maasvlakte), Netherlands'
+    location: 'Rotterdam (Maasvlakte), Netherlands',
+    from: 'March 2012',
+    to: 'July 2012'
   },
   {
     id: 6,
     title: 'Business Development Intern',
+    image: '',
     description: 'Implementation of a cross-country CRM solution',
     organization: 'Europe Container Terminals',
-    location: 'Rotterdam (Maasvlakte), Netherlands'
+    location: 'Rotterdam (Maasvlakte), Netherlands',
+    from: 'January 2012',
+    to: 'March 2012'
   }
 
 ];
