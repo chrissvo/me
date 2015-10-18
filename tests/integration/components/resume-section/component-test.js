876fbcbad396ea2cac141aba51e3f19f1d373971
Chrissvo <git@christian.surf>
2015-10-18 17:01:51 +0200

    Create resume-section component

---
+++ tests/integration/components/resume-section/component-test.js
@@ -0,0 +1,26 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('resume-section', 'Integration | Component | resume section', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{resume-section}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#resume-section}}
+      template block text
+    {{/resume-section}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
