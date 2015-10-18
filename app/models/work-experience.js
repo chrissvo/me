6e5c9e5199efd441c207ebcd42e6dbdeb2d8c090
Chrissvo <git@christian.surf>
2015-10-18 20:20:05 +0200

    Add image to work-experience model

--- app/models/work-experience.js
+++ app/models/work-experience.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 
   title: DS.attr('string'),
+  image: DS.attr('string'),
   description: DS.attr('string'),
   organization: DS.attr('string'),
   from: DS.attr('string'),
