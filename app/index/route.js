9fe9f824cb8082c95ef1baaca4359e7658f1172e
Chrissvo <git@christian.surf>
2015-10-27 21:54:47 +0100

    Remove model from index route

--- app/index/route.js
+++ app/index/route.js
@@ -2,26 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  model: function() {
-
-    const workExperiences = new Ember.RSVP.Promise(function(resolve) {
-      return resolve(this.store.findAll('work-experience'));
-    }.bind(this));
-
-    const educationalAchievements = new Ember.RSVP.Promise(function(resolve) {
-      return resolve(this.store.findAll('educational-achievement'));
-    }.bind(this));
-
-    const extracurricularActivities = new Ember.RSVP.Promise(function(resolve) {
-      return resolve(this.store.findAll('extracurricular-activity'));
-    }.bind(this));
-
-    return Ember.RSVP.hash({
-      workExperiences: workExperiences,
-      educationalAchievements: educationalAchievements,
-      extracurricularActivities: extracurricularActivities
-    });
-
-  }
-
 });
