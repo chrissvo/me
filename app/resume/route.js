83581e0ee5d435628bf902839edb3a24acbd36e4
Chrissvo <git@christian.surf>
2015-10-27 21:56:04 +0100

    Return resume-sections from the resume route

--- app/resume/route.js
+++ app/resume/route.js
@@ -4,24 +4,14 @@ export default Ember.Route.extend({
 
   model: function() {
 
-    const workExperiences = new Ember.RSVP.Promise(function(resolve) {
-      return resolve(this.store.findAll('work-experience'));
-    }.bind(this));
-
-    const educationalAchievements = new Ember.RSVP.Promise(function(resolve) {
-      return resolve(this.store.findAll('educational-achievement'));
-    }.bind(this));
-
-    const extracurricularActivities = new Ember.RSVP.Promise(function(resolve) {
-      return resolve(this.store.findAll('extracurricular-activity'));
+    const resumeSections = new Ember.RSVP.Promise(function(resolve) {
+      return resolve(this.store.findAll('resume-section'));
     }.bind(this));
 
     return Ember.RSVP.hash({
-      workExperiences: workExperiences,
-      educationalAchievements: educationalAchievements,
-      extracurricularActivities: extracurricularActivities
+      resumeSections: resumeSections
     });
 
   }
-  
+
 });
