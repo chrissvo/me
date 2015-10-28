4d7f7157d46164f37ea3140744f5b3fa6a2b8832
Chrissvo <git@christian.surf>
2015-10-28 21:24:28 +0100

    Add an even-helper

---
+++ tests/unit/helpers/even-test.js
@@ -0,0 +1,10 @@
+import { even } from '../../../helpers/even';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | even');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = even(42);
+  assert.ok(result);
+});
