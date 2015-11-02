d38a9c5977a5445d2e10493f44540e2a30b7b580
Chrissvo <git@christian.surf>
2015-11-02 18:17:04 +0100

    Fix styling resume route on mobile

--- app/components/resume-section/component.js
+++ app/components/resume-section/component.js
@@ -3,32 +3,35 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   didRender: function() {
-    $(window).scroll(function() {
+    if (!this.get('isPhone')) {
+      $(window).scroll(function() {
 
-      $('.resume-section').each(function() {
+        $('.resume-section').each(function() {
 
-        const header = $('.resume-header');
-        const resume = $(this);
-        const windowTop = 0;
-        const headerTop = header.offset().top ;
-        const resumeTop = resume.offset().top;
-        const headerBottom = headerTop + header.height() + 40;
-        const resumeBottom = resumeTop + resume.height();
+          const header = $('.resume-header');
+          const resume = $(this);
+          const windowTop = 0;
+          const headerTop = header.offset().top ;
+          const resumeTop = resume.offset().top;
+          const headerBottom = headerTop + header.height() + 40;
+          const resumeBottom = resumeTop + resume.height();
 
-        const sectionId = Number($(this).attr('id'));
-        const nextSectionId = sectionId + 1;
-        const previousSectionId = sectionId - 1;
+          const sectionId = Number($(this).attr('id'));
+          const nextSectionId = sectionId + 1;
+          const previousSectionId = sectionId - 1;
 
-        if (headerBottom  >= resumeTop && windowTop < resumeBottom) {
-          $('#' + nextSectionId).addClass('first');
-          $('#' + nextSectionId).removeClass('middle');
-          return $(this).css('opacity', 0);
-        }
-        $('#' + nextSectionId).removeClass('first');
-        $('#' + nextSectionId).addClass('middle');
-        return $(this).css('opacity', 1);
+          if (headerBottom  >= resumeTop && windowTop < resumeBottom) {
+            $('#' + nextSectionId).addClass('first');
+            $('#' + nextSectionId).removeClass('middle');
+            return $(this).css('opacity', 0);
+          }
+          
+          $('#' + nextSectionId).removeClass('first');
+          $('#' + nextSectionId).addClass('middle');
+          return $(this).css('opacity', 1);
+        });
       });
-    });
+    }
   }
 
 });
